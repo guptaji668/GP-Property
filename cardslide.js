41bd6865140c5b1875a1933c2bfd847787869c07
Rohan Gupta <122445469+guptaji668@users.noreply.github.com>
2023-06-09 11:17:16 +0530

    Add files via upload

---
+++ cardslide.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function(){
+    $('#slidecard').hide()
+   
+   
+    $('#slidecard').slideDown(2000)
+    
+    // $('#closebtn').click(function(){
+    //    
+    // })
+
+    $('#staticBackdrop').open('modal')
+
+})
+// const allk=()=>{
+//     return (
+//       "fnjhkjfn"  
+//     )
+// }
+
+// const btn = document.getElementById('random');
+// btn.addEventListener('click', allk);
+
+
+
+
+
